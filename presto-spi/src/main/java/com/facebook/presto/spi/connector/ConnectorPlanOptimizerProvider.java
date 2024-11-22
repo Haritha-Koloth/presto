9c29c9f6ceee45d66260b4f208dfe2cff5dfcea3
Haritha Koloth <HARITHA.K@ibm.com>
2024-11-22 10:31:56 +0530

    Join Pushdown SPI and Core changes
    Join Pushdown SPI and Core changes
    
    Co-Authored-By: Ajas M <Ajas.M@ibm.com>
    Co-Authored-By: Glerin Pinhero <glerin.pinhero@ibm.com>
    Co-Authored-By: Thanzeel Hassan <thanzeel.hassan@ibm.com>
    Co-Authored-By: Anant Aneja <anant.aneja@ibm.com>

--- presto-spi/src/main/java/com/facebook/presto/spi/connector/ConnectorPlanOptimizerProvider.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/connector/ConnectorPlanOptimizerProvider.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.spi.connector;
 
 import com.facebook.presto.spi.ConnectorPlanOptimizer;
+import com.google.common.collect.ImmutableSet;
 
 import java.util.Collections;
 import java.util.Set;
